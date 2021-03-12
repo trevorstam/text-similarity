00b840197f2c1625c4a247b91f4a4b592e274304
trevorstam <trevorstam@gmail.com>
2021-03-12 14:13:18 -0800

    create tables script, tune queries, set up configuration for db

--- sql_queries.py
+++ sql_queries.py
@@ -1,25 +1,25 @@
 # DROP TABLES
-drop_input_texts = " DROP TABLE IF EXISTS input_texts"
+drop_input_texts = "DROP TABLE IF EXISTS input_texts"
 drop_textsim_metrics = "DROP TABLE IF EXISTS textsim_metrics"
 
 # CREATE TABLES
 
 create_input_texts = """
   CREATE TABLE IF NOT EXISTS input_texts(
-    table_id INT IDENTITY(1, 1) NOT NULL,
+    table_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     text_id VARCHAR NOT NULL,
     text_name VARCHAR,
     total_words INT,
     unique_words INT,
     total_words_cleaned INT,
     unique_words_cleaned INT,
-    ingestion_time DATETIME2
+    ingestion_time TIMESTAMP
   )
 """
 
 create_textsim_metrics = """
   CREATE TABLE IF NOT EXISTS textsim_metrics (
-    ts_id INT IDENTITY(1, 1) NOT NULL,
+    ts_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
     text_id_1 VARCHAR NOT NULL,
     text_name_1 VARCHAR,
     text_id_2 VARCHAR,
